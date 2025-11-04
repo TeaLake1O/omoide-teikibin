280f506bcb9d2413d9e3012ccc8f559b7e3a5f42
hiro <pracube0818@gmail.com>
2025-11-04 14:51:21 +0900

    test

--- omoide_teikibin_project/friend/models.py
+++ omoide_teikibin_project/friend/models.py
@@ -7,21 +7,24 @@ class Friendship(models.Model):
         PENDING_A2B = "A2B", "A→B申請"
         PENDING_B2A = "B2A", "B→A申請"
         ACCEPTED = "ACPT", "承認"
-        
-    friend_id = models.AutoField(primary_key=True,verbose_name="フレンド")
-    username_a = models.ForeignKey(CustomUser,verbose_name="ユーザーA", related_name = "username_a", on_delete = models.CASCADE)
-    username_b = models.ForeignKey(CustomUser,verbose_name="ユーザーB", related_name = "username_b", on_delete = models.CASCADE)
     
-    status = models.CharField(max_length=4, choices=Status.choices, default=Status.PENDING_A2B)
+    
+    friend_id = models.AutoField(primary_key=True,verbose_name="フレンド")
+    username_a = models.ForeignKey(CustomUser,verbose_name="ユーザーA", related_name = "friendship_as_a", on_delete = models.CASCADE)
+    username_b = models.ForeignKey(CustomUser,verbose_name="ユーザーB", related_name = "friendship_as_b", on_delete = models.CASCADE)
+    
+    status = models.CharField(max_length = 4, )
+    
+    """
+    user_a_request = models.BooleanField(default = False,verbose_name="ユーザA申請")
+    user_b_request = models.BooleanField(default = False,verbose_name="ユーザB申請")
+    """
     
     created_at = models.DateTimeField(auto_now_add = True,verbose_name="作成日時")
-    friend_date = models.DateTimeField(auto_now_add = True, verbose_name = "フレンド成立日")
-    
-    
+    friend_date = models.DateTimeField(auto_now = True, verbose_name = "フレンド成立日")
 
 class Message(models.Model):
     message_id = models.AutoField(primary_key = True)
-    friend_id = models.ForeignKey(Friendship)
     message_image = models.ImageField()
     message_text = models.TextField()
     deleted_at = models.DateTimeField()
