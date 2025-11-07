7bafa6dc21a39aa9eafa6bdbf49529299b790538
hiro <pracube0818@gmail.com>
2025-11-07 13:46:23 +0900

    mediaurlの追加

--- omoide_teikibin_project/friend/views.py
+++ omoide_teikibin_project/friend/views.py
@@ -1,10 +1,10 @@
-from django.views.generic import ListView, View, FormView, UpdateView
+from django.views.generic import ListView, View, FormView, UpdateView, CreateView
 from .models import Friendship
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 
 
-class FriendView(ListView,LoginRequiredMixin):
+class FriendView(LoginRequiredMixin, ListView):
     model = Friendship
     
     template_name = "friend_view.html"
@@ -12,13 +12,15 @@ class FriendView(ListView,LoginRequiredMixin):
     
     def get_queryset(self):
         user = self.request.user
-        return (
+        
+        f = (
             Friendship.objects
             .filter(Q(username_a = user) | Q(username_b = user))
             .filter(status = "ACPT")
             .select_related("username_a", "username_b")
-            .order_by("")
+            .order_by("-friend_date")
         )
+        return f
 
 class FriendRequestView(UpdateView,LoginRequiredMixin):
     def get(self, request):
