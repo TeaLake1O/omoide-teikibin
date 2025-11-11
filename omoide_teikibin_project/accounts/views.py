6c98362ea8e03508e499667d20996942454f6b6c
inakappe-112 <keigo2680@gmail.com>
2025-11-11 11:20:21 +0900

    パスワード認証更新

--- omoide_teikibin_project/accounts/views.py
+++ omoide_teikibin_project/accounts/views.py
@@ -59,31 +59,47 @@ class UserInfoView(DetailView):
     def get_context_data(self, **kwargs):
         # contextの取得
         context = super().get_context_data(**kwargs)
-
         
         return context
     
-# class PasswordCheck(FormView):
+class PasswordCheck(FormView):
+    '''パスワード確認ページのビュー
+    '''
+    # レンダリングするテンプレート
+    template_name = "password_check.html"
+    # forms.pyで定義したフォームのクラス
+    form_class = PasswordCheckForm
+    # パスワード認証後のリダイレクト先のURLパターン
+    success_url = reverse_lazy('accounts:signup_success')
+    
+    
+    def form_valid(self, form):
+        # 入力を取得
+        pass_text = self.request.POST.get("password", "")
+        
+        print(self.request.META.get('HTTP_REFERER'))
+        
+        # パスワードが正しいかチェック
+        if self.request.user.check_password(pass_text):
+            print('ok')
+            return super().form_valid(form)
+        else:
+            print('dame')
+            return super().form_invalid(form)
+            
+            
+            
+    
+# class PasswordCheck(TemplateView):
 #     '''パスワード確認ページのビュー
 #     '''
-#     # forms.pyで定義したフォームのクラス
-#     form_class = PasswordCheckForm
-#     success_url = reverse_lazy('accounts:signup_success')
 #     # レンダリングするテンプレート
 #     template_name = "password_check.html"
     
-#     def form_valid(self, form):
-#         print(1)
-#         return super().form_valid(form)
-
-class PasswordCheck(View):
-
-    template_name = "password_check.html"
-
-    def post(self, request):
-            pass_text = request.POST.get("password", "")
+#     def post(self, request):
+#             pass_text = request.POST.get("password", "")
            
-            if request.user.check_password(pass_text):
-                print("ok")
-            else:
-                print("dame")
+#             if request.user.check_password(pass_text):
+#                 print("ok")
+#             else:
+#                 print("dame")
