80e3fbc6e188623e63264c9bf2bad161d3afc86d
inakappe-112 <keigo2680@gmail.com>
2025-11-18 14:20:48 +0900

    モデルの追加とmigrate

--- omoide_teikibin_project/accounts/models.py
+++ omoide_teikibin_project/accounts/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -8,7 +9,6 @@ def gen_image_path_customuser(instance, filename):
     newname = f"user_icon.{ext}"
     return f"users/{instance.username}/{newname}"
 
-
 class CustomUser(AbstractUser):
     deleted_at = models.DateTimeField(blank = True, null = True, verbose_name = "削除日時")
     nickname = models.CharField(blank = True, null  =True, max_length = 30, verbose_name = "ニックネーム")
@@ -16,8 +16,8 @@ class CustomUser(AbstractUser):
     user_icon = models.ImageField(null = True, blank= True, verbose_name = "アイコン", upload_to = gen_image_path_customuser)
     email = models.EmailField(unique = True,max_length=254, verbose_name = "メールアドレス")
 
-
-
-
-
-
+class NewEmail(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    new_email = models.EmailField()
+    token = models.UUIDField(default=uuid.uuid4, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
