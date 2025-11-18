7178a15345e0310e0ae8c63505c3ab05970e688a
inakappe-112 <keigo2680@gmail.com>
2025-11-18 14:53:12 +0900

    NewEmailを設定

--- omoide_teikibin_project/accounts/admin.py
+++ omoide_teikibin_project/accounts/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import CustomUser
+from .models import CustomUser, NewEmail
 
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'user_icon',"email")  # iconを表示
     list_display_links = ('id', 'username')
+    
+class NewEmailAdmin(admin.ModelAdmin):
+    list_display = ('user', 'new_email', 'token')
+
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(NewEmail, NewEmailAdmin)
