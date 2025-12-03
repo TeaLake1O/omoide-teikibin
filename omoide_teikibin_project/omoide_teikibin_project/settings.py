fda9d911026682d38501a2dcec288b1930d3df98
hiro <pracube0818@gmail.com>
2025-12-03 14:45:15 +0900

    cors関連の設定

--- omoide_teikibin_project/omoide_teikibin_project/settings.py
+++ omoide_teikibin_project/omoide_teikibin_project/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = 'django-insecure-_hc$^fumw86*^7w$mbk%31mi%yu+vb6!)tb2)cm!i@+rs(3e$r
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOW_CREDENTIALS = True
 
 
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    'corsheaders',
     
     "accounts",
     "post",
@@ -46,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
