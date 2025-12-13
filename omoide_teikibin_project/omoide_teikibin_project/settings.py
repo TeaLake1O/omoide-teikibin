c145465fc8d32e63670a8401f09921c0184df90b
hiro <pracube0818@gmail.com>
2025-12-13 23:22:46 +0900

    クロスサイトのクッキー関連の記述を追加

--- omoide_teikibin_project/omoide_teikibin_project/settings.py
+++ omoide_teikibin_project/omoide_teikibin_project/settings.py
@@ -34,7 +34,10 @@ CSRF_TRUSTED_ORIGINS = [
     "https://omoide-teikibin-front.vercel.app",
 ]
 
-
+SESSION_COOKIE_SAMESITE = "None"
+CSRF_COOKIE_SAMESITE = "None"
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Application definition
 
